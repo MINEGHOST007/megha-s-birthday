88189b8cd5a7391ffa845c5bd89212d85a7fae86
Suravaram Sankeerth Reddy <ssr.22u10038@btech.nitdgp.ac.in>
2023-05-17 19:01:56 +0530

    its been a while it was for someone but they couldnt see the effort atleast the world see it completed on 21st march 2023

---
+++ insanehardwork.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main(){
+    int c,a;
+    printf("its been 20 days that ive been working on this\n");
+    printf("and when everything is done this just doesn't get hosted on the web :-) \n");
+    printf("my mind - its not that great too right you just made a greeting card!! \n");
+    printf("enter 1 if you want to dare if not press 0 \n");
+    scanf("%d",&c);
+    if(c==0)
+    printf(" heart - well i know you tried your best but dont give up now \n");
+    printf("enter 1 if you want to dare if not press 0 \n ");
+    scanf("%d",&a);
+    if(a==1)
+    printf(" good choice execute the command npm run dev \n");
+}
